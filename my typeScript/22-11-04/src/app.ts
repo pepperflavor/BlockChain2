a50e1f8849d5b0df0b2fdea058a7c8170e719d3a
pepperflavor <pepperflavor@gmail.com>
2022-11-03 17:58:03 +0900

    class

---
+++ my typeScript/22-11-04/src/app.ts
@@ -0,0 +1,14 @@
+class Department {
+    name: string;
+
+    constructor(n: string){
+        this.name = n; // Department객체가생성될때 name의 값을 n에서 얻는다
+    }
+}
+
+const accounting = new Department('Accounting');
+console.log(accounting);
+
+//# sourceMAppingURL=app.js.map
+
+
