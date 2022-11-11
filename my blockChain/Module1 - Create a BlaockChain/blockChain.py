c793cded513b3d04cef9ebc8f39c0c7d661a116b
pepperflavor <j8747j@naver.com>
2022-11-11 18:21:41 +0900

    Python - 체인유효성 검사함수 추가

--- my blockChain/Module1 - Create a BlaockChain/blockChain.py
+++ my blockChain/Module1 - Create a BlaockChain/blockChain.py
@@ -35,7 +35,7 @@ class BlockChain:
         return self.chain[-1]
 
     # 작업증명 함수 - 채굴자가 해당 블록의 최초 채굴자인지 확인해야함
-    def proof_of_block(self, previous_proof):
+    def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
         while check_proof is False:
@@ -58,5 +58,28 @@ class BlockChain:
         encoded_block = json.dumps(block, sort_keys= True).encode()
         # .hexdigest() 함수를 통해서 16진수 형식으로 값을 도출해냄
         return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        # 인덱스의 첫번째값이 0 이 아닌 1로 만들어줌
+        block_index = 1
+        while block_index < len(chain):
+            current_block = chain[block_index]
+            # 여기서 hash는 현재 다루는 블록체인의 메서드이기 때문에
+            # 이전블록에 대해서는 self.hash로 불러와야한다
+            if current_block['previous_hash'] != self.hash(previous_block):
+                return False
+            # proof_of_work에서 작업한 선행 문자열4개가 '0000'인지 확인
+            # 현재블록의 증명키를 가져와서 현재증명을 불러온다
+            previous_proof = previous_block['proof']
+            proof = current_block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            # 다음 블록의 검증 과정에서는 방금 검증한 current블록이 pre블록이 되어야한다. 그 설정을 해주는 것
+            previous_block = current_block
+            block_index += 1
+        return True
+             
     
 # 2 - mining blockChain
