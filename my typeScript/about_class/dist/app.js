b2b3de207b5d9b7b7ea0bb499684ea1381297279
pepperflavor <j8747j@naver.com>
2022-11-15 12:41:43 +0900

    class 정리중

---
+++ my typeScript/about_class/dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+class Department {
+ 
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accounting = new Department('Accounting');
+console.log(accounting);
+//# sourceMappingURL=app.js.map
+
---
+++ my typeScript/about_class/src/app.ts
@@ -0,0 +1,63 @@
+class Department{
+    name : string;
+
+    constructor( n: string){
+        this.name = n;
+    }
+    
+    describe(){
+        console.log('Department : '+ this.name);
+        
+    }
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
+
+accounting.describe(); // accounting 이 출력된다.
+
+// 그렇다면 다음코드의 출력 결과물은?
+const accountingCopy = { describe : accounting.describe };
+
+accountingCopy.describe();
+
+// 결과 => undifine
+
+/*
+    컴파일 에러는 발생하지않지만 런타임에서는 에러가 발생한다.
+    더미 객체로 생성되었기 때문인데
+    왜 ?  accountingCopy 이 객체에 describe 속성값을 부여했기 때문이다
+
+    그리고 describe의 속성값은 
+    new Department로 생성한 accounting 객체의 describe를 가리키고 있으면서 () 가 없어서
+    함수를 실행하지는 않으므로 함수자체만 전달해준다.
+
+    그리고 
+    accountingCopy.describe(); 에서
+    describe 함수를 호출해 실행해준다. 
+    
+    그러나 
+    Department class에 정의된 describe 함수의 내용 중 this.name을 참조하지 않는다.
+    
+    왜냐?
+    accountingCopy 라는 객체에는 name이라는 속성이 없는 객체이기 때문이다.
+
+*/
+
+// 클래스에 기반한 인스턴스를 참조하도록 수정한 것
+
+class Department_2{
+    name : string;
+
+    constructor( n: string){
+        this.name = n;
+    }
+    // 타입을 해당 클래스로 지정해줘야한다. 타입스크립트는 this가 무엇으로 참조되어야 하는지 인식하기 때문
+    describe(this : Department_2){
+        console.log('Department : '+ this.name);
+        
+    }
+}
+
+
