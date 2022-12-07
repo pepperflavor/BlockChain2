67d72177c3059c00fc9a8fdcf19806bcb924d838
pepperflavor <j8747j@naver.com>
2022-12-07 18:18:41 +0900

    트랜잭션 블록에 등록

--- my blockChain/Module2-Plan of Attack/hadcoin.py
+++ my blockChain/Module2-Plan of Attack/hadcoin.py
@@ -56,7 +56,7 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-    
+
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
@@ -90,14 +90,17 @@ class Blockchain:
         # 리스트 형태가 아니기 때문에 add를 사용했다
         self.nodes.add(parsed_url.netloc)
 
-    #특정 노드에서 호출할 함수 노드들중 가장 긴 체인이 진짜 체인이 되어야 하니까
+    #특정 노드에서 호출할 함수 노드들중 가장 긴 진짜 체인을 판별해 교체하는 함수
     def replace_chain(self):
         network = self.nodes
         longest_chain = None
+        # 현재 체인의 길이, 다른 체인과 비교후 더 길면 교체해줄 예정
         max_length = len(self.chain)
         for node in network:
+            # *주의* response = requests.get('http://127.0.0.1:5000/get_chain') => 이렇게 적으면 5000번을 사용하는 하나의 노드만 가리키는 것이다. 
+            # nodes에 담긴 노드들 전부를 훑어야 하기 때문에 response = requests.get(f'http://{node}/get_chain') 이렇게 지정해줘야한다.
             # request 라이브러리를 사용 get() /get_chain 요청의 응답을 얻어옴
-            #response = requests.get('http://127.0.0.1:5000/get_chain') => 이렇게 적으면 5000번을 사용하는 하나의 노드만 가리키는 것이다. nodes에 담긴 노드들 전부를 훑어야 하기 때문에
+            # f 문자열 구문. 파이썬 3.6버전 사용가능
             response = requests.get(f'http://{node}/get_chain')
             # 정상작동하는지 확인
             if response.status_code == 200:
@@ -119,6 +122,10 @@ class Blockchain:
 # Creating a Web App
 app = Flask(__name__)
 
+# Creating an address for the node on Port 5000
+# uuid4는 무작위 고유 주소 생성, 노드에게 무작위 주소를 만들어준다.
+node_address = str(uuid4()).replace('-', '')
+
 # Creating a Blockchain
 blockchain = Blockchain()
 
@@ -129,18 +136,21 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    # add_transaction 함수 참고해서 인수 넣어주기
+    blockchain.add_transaction(sender= node_address, receiver= 'Hajin', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash'],
+                'transaction' : block['transactions']}
     return jsonify(response), 200
 
 # Getting the full Blockchain
 @app.route('/get_chain', methods = ['GET'])
 def get_chain():
-    # get_chain의 응답
+    # get_chain의 응답을 다음과 같은 객체로 받겠다는 뜻
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
@@ -156,9 +166,32 @@ def is_valid():
         response = {'message': 'Human, we have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
+# Adding a new transaction to the Blockchain
+@app.route('/add_transaction', methods = ['POST'])
+def Add_transaction():
+    # 트랜잭션을 발생시키는 함수이므로 발신자와 수신자 그리고 애드코인 거래량이 포함되는 트랜잭션을 생성해야한다
+    # get_json : postman에 기재되는 json 파일을 가져올 수 있다.
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    # transaction_keys 로 선언한 요소들이 json 객체 내에서 발견되지 않으면 에러를 반환하도록하는 함수
+    # http code 참고
+    if not all (key in json for key in transaction_keys):
+        return "Some elements of the transaction atr missing", 400
+    # 누락된 키가 없는경우라면 해당 트랜잭션을 블록에 추가해준다. 키순서도 중요
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message' : f"This transaction will be added to Block {index}"}
+    # 인수를 json 파일 형식으로 반환해준다
+    return jsonify(response), 201
+
+
 # Running the app
 # flask의 포트 번호는 5000번이다
 app.run(host = '0.0.0.0', port = 5000)
 
 
 # Part 3 - 합의 적용
+#Connecting new nodes
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    json.get('')
--- my blockChain/Module2-Plan of Attack/memo_function.js
+++ my blockChain/Module2-Plan of Attack/memo_function.js
@@ -7,4 +7,28 @@
         network = self.nodes -> 이 변수는 해당 체인에 속한 노드들의 집합이다.
         longest_chain = None // 채굴자가 잇을 테니 그중 가자ㅓㅇ 긴 체인
         max_length = len(self.chain) // 현재 체인의 길이
+
+
+    f 문자열
+    일반적인 문자열 앞에 f 또는 F 문자를 붙여주면 f-string이 된다.
+    {}를 사용하면 표현식 및 변수를 사용할 수 있다. 즉, 함수나 변수 값을 삽입하는 용도로 사용할 수 있다는 말이다!
+    +) 객체도 문자열로 출력가능
+
+
+    GET POST 요청의 차이
+    - 응답을 받을 때 무언가를 생성해야 하는지 여부에 달려있다.
+    ex) 블록을 찾을때 -> get
+        블록을 등록할때 -> post
+*/
+
+/*
+    #Connecting new nodes
+    @app.route('/connect_node', methods = ['POST'])
+    def connect_node():                             
+    json = request.get_json()                       
+    json.get('')
+    
+    // 인수가 필요없는데 본 블록체인의 변수만이 필요하며 이미 블록체인의
+
+
 */
